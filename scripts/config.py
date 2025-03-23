4f9058e12d68e0ea1cb317841c62b3ff8a0e2675
DJ-BEZJOP <sdfghjkiuy@proton.me>
2025-03-23 00:48:38 -0500

    Remove old files

--- scripts/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-import os
-GEMINI_API_KEY = 'AIzaSyDwzw55cXckSXagBLYiu87DEPXW2LqC1_0'  # Replace with your real Gemini API key
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # Корень проекта
-INPUT_DIR = os.path.join(BASE_DIR, 'input')
-OUTPUT_DIR = os.path.join(BASE_DIR, 'output')
